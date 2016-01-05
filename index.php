e1e77087a2beadc058cd66531225cdf630b87124
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-01-05 20:06:13 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<html>
+	<head>
+		<title>Strona Michała Krzysztofa Feilera na serwerze Platinum</title>
+		<meta charset="UTF-8">
+		<!--script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script-->
+		<link rel=stylesheet" type="text/css" href="/styles.css">
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+<link rel="icon" type="image/png" href="DSCF7629-bfab-fixed-squared-666px.png">
+	</head>
+	<body><!-- Google Tag Manager -->
+		<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KP8WW2"
+		    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		    })(window,document,'script','dataLayer','GTM-KP8WW2');</script>
+		    <!-- End Google Tag Manager -->
+		    <!--p>To jest strona Michała Krzysztofa Feilera na serwerze Platinum.</p-->
+<img src="DSCF7629-bfab-fixed-squared-666px.png" height="333px">
+		<br><hr>
+		<p><a href="ArchieT.key">Mój klucz publiczny PGP<br><code>72A3 D939 ECA2 67FB 0096  8F4B 8179 43C5 88A5 0062</code></a></p>
+		<hr>
+		<p><a href="mailto:Micha%C5%82%20Krzysztof%20Feiler<archiet@platinum.edu.pl>">Michał Krzysztof Feiler &lt;archiet@platinum.edu.pl&gt;</a></p>
+		<hr><!--
+		<p><a href="https://keybase.io/ArchieT">ArchieT on Keybase</a></p>
+		<p><a href="https://google.com/+MichalKrzysztofFeiler">My Google+ profile</a></p>
+		<p><a href="https://github.com/ArchieT">ArchieT on Github</a></p>
+		<p><a href="https://linkedin.com/in/feilermichal">My LinkedIn profile</a></p>-->
+		<p><table class="profiles"><tr class="profiles">
+				<td class="prof"><a class="prof" href="https://keybase.io/ArchieT">
+						<img class="prof" width="64" src="https://keybase.io/images/logo_no_text.png">
+					</a></td>
+					<td class="prof"><!-- Place this code where you want the badge to render. -->
+						<a class="prof" href="//plus.google.com/u/0/100176969216547798972?prsrc=3"
+							   rel="publisher" target="_top">
+							<img class="prof" width="64" src="//ssl.gstatic.com/images/icons/gplus-64.png" alt="Google+"/>
+						</a></td>
+					<td class="prof"><a class="prof" href="https://github.com/ArchieT">
+							<img class="prof" width="64" src="http://cdn.flaticon.com/png/256/25231.png">
+						</a></td>
+						<td class="prof"><a class="prof" href="https://linkedin.com/in/feilermichal">
+								<img class="prof" width="64" src="http://cdn.marketingboard.biz/wp-content/uploads/2014/08/LinkedIn-Badge.png">
+							</a></td>
+			</tr></table></p>
+		<hr>
+<p><a href="https://medium.com/@ArchieT">My Medium profile</a></p><hr>
+<br><!--<div class="snowden-g-post"><div class="g-post" data-href="https://plus.google.com/+MichalKrzysztofFeiler/posts/3EuedCv8o3n"></div></div-->
+			<div class="snowden"><p class="snowden"><i>"— Arguing that you don’t care about the right to privacy because you have nothing to hide, is no different than saying you don’t care about free speech because you have nothing to say, or the freedom of press because you’re not a journalist, or the freedom of religion because you’re not a Christian. Rights in societies are collective, and individual. You can’t give away the rights of a minority, even if you vote as a majority. Rights are inherent to our nature, they’re not granted by governments, they’re guaranteed by governments. They’re protected by governments."</i></p>
+<p class="signsnowden" align="right">— Edward Snowden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+<p align="right"><font size="2"><a href="http://fokus.dn.se/edward-snowden-english/">in a Dagens Nyheter interview in Moscow, on the 6th of November, 2015</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</font></p></div>
+<br><br><hr><figure><embed src="https://wakatime.com/@ArchieT/a27d3166-6824-4679-9dc9-f4e329e5ac5b.svg"></embed></figure><br><figure><embed src="https://wakatime.com/@ArchieT/79ec7a96-7a2e-47c4-a2ff-924926a0ba98.svg"></embed></figure><br><figure><embed src="https://wakatime.com/@ArchieT/1da1c520-adbe-499f-af18-726392af7b62.svg"></embed></figure>
+<br><figure><embed src="https://wakatime.com/@ArchieT/f858bddd-405a-42d8-aba2-f109cdc1d804.svg"></embed></figure>
+	</body>
+</html>
