1643a5bd9e429c464c2173da2c618d5f65a13707
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-05-05 19:57:47 +0200

    legend: right-center, uncommented chartArea and absolute positioning

--- wakatimeget.js
+++ wakatimeget.js
@@ -48,8 +48,8 @@ function langues(id) {
 			chart.draw(google.visualization.arrayToDataTable(dalist), {
 				title: nasz.title,
 				legend: {
-					position: "labeled",
-					alignment: "end",
+					position: "right",
+					alignment: "center",
 				},
 				fontSize: 12,
 				fintName: "Inconsolata",
@@ -57,16 +57,12 @@ function langues(id) {
 				colors: ['#8dd3c7', '#ffffb3', '#bebada', '#fb8072', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9', '#bc80bd', '#ccebc5', '#ffed6f'],
 				pieSliceBorderColor: "#aaaaaa",
 				pieHole: 0.7,
-				/*
 				chartArea: {
 					height: "100%",
 					width: "100%",
-					
 					top: 0,
 					left: 0,
-					
 				},
-				*/
 			});
 		},
 		error: function(err) {
