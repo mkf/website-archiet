55e28abcb3aa7fb1ea1829fa51e732154809d094
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-05-05 19:42:49 +0200

    Inconsolata

--- wakatimeget.js
+++ wakatimeget.js
@@ -51,7 +51,7 @@ function langues(id) {
 					position: "left",
 				},
 				fontSize: 12,
-				fintName: "Courier New",
+				fintName: "Inconsolata",
 				pieSliceText: 'none',
 				colors: ['#8dd3c7', '#ffffb3', '#bebada', '#fb8072', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9', '#bc80bd', '#ccebc5', '#ffed6f'],
 				pieSliceBorderColor: "#aaaaaa",
