11727a860dd9f10f38bbcb91a1fcc2a79b05e545
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-05-05 20:16:18 +0200

    langues pieChart pieHole 0.5

--- wakatimeget.js
+++ wakatimeget.js
@@ -60,7 +60,7 @@ function langues(id) {
 				},
 				colors: ['#8dd3c7', '#ffffb3', '#bebada', '#fb8072', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9', '#bc80bd', '#ccebc5', '#ffed6f'],
 				pieSliceBorderColor: "#aaaaaa",
-				pieHole: 0.7,
+				pieHole: 0.5,
 				chartArea: {
 					height: "100%",
 					width: "100%",
