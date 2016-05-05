7547cfdd96991e2e1088df6faeae7d160ad6bcaa
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-05-05 19:36:15 +0200

    Colors of piecharts

--- wakatimeget.js
+++ wakatimeget.js
@@ -51,7 +51,9 @@ function langues(id) {
 					position: "left",
 				},
 				fontSize: 8,
-				pieSliceText: 'value',
+				pieSliceText: 'none',
+				colors: ['#8dd3c7', '#ffffb3', '#bebada', '#fb8072', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9', '#bc80bd', '#ccebc5', '#ffed6f'],
+				pieSliceBorderColor: "black",
 				pieHole: 0.5,
 				chartArea: {
 					height: "100%",
